2e71533a30907978fc0bccd2a0cfa344fa5500aa
CodeBishop <sbishop@longdays.com>
2017-11-13 22:12:00 -0800

    Experiment with a menu system in tinker.py.

--- tinker.py
+++ tinker.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 import curses
 
+SELECTOR_ABSENT = -1
+
 
 def main(stdscr):
+    curses.curs_set(0)  # Make the terminal cursor invisible.
+    selector = SELECTOR_ABSENT
+    drives = ['drive 1', 'drive 2', 'drive 3']
+
     exitFlag = False
     while not exitFlag:
+        # Draw the view.
         stdscr.clear()
         stdscr.border(0)
-        stdscr.addstr(2, 2, "Please enter a number...")
-        stdscr.addstr(4, 4, "1 - Add a user")
-        stdscr.addstr(4, 4, "2 - Restart Apache")
-        stdscr.addstr(6, 4, "3 - Show disk space")
-        stdscr.addstr(7, 4, "4 - Exit")
+        for y in range(len(drives)):
+            stdscr.addstr(4 + y, 8, drives[y])
+        if selector is not SELECTOR_ABSENT:
+            stdscr.addstr(4 + selector, 4, "-->")
         stdscr.refresh()
 
-        x = stdscr.getch()
+        # Get user input.
+        userKey = stdscr.getch()
+        if len(drives) == 0:
+            selector = SELECTOR_ABSENT
+        else:
+            if userKey == curses.KEY_DOWN:
+                selector = (selector + 1) % len(drives)
+            if userKey == curses.KEY_UP:
+                selector = (selector - 1) % len(drives)
 
-        if x != '':
+        if userKey == ord('q'):
             exitFlag = True
 
 curses.wrapper(main)
