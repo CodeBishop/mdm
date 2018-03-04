4d0c5c37b12d92b1dbfdf3a1b1e23fb23708f52b
codebishop <sbishop@longdays.com>
2018-03-03 18:12:11 -0800

    Experiment with color printing in utils.py.

--- mdmSMART/utils.py
+++ mdmSMART/utils.py
@@ -1,15 +1,55 @@
 #!/usr/bin/env python
 
-# A collection of utility functions for MDM (Multi-Drive Manager)
+# A collection of utility functions and classes for MDM (Multi-Drive Manager)
 import os
 import re
 import subprocess
+import curses
 
 # Utility function constants.
 SEARCH_FAILED = -1
 
 # Open the null device for dumping unwanted output into.
 DEVNULL = open(os.devnull, 'w')
+
+CEC = "%%%"  # Escape code for colored or special text.
+CECLEN = len(CEC) + 2  # Total length of an escape code.
+
+# Prepare color pairs.
+# colorPair = list()
+# for i in range(0, 8):
+#     colorPairSublist = list()
+#     for j in range():
+#         colorPairSublist.append
+
+
+class ColoredString:
+    def __init__(self, text):
+        self.text = text
+
+    def length(self):
+        return len(self.text) - self.text.count(CEC) * CECLEN
+
+
+def printAt(window, x, y, text):
+    window.addStr
+    curses.use_default_colors()
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i + 1, i, i-1)
+    # stdscr.addstr(0, 0, '{0} colors available'.format(curses.COLORS))
+    # maxy, maxx = stdscr.getmaxyx()
+    # maxx = maxx - maxx % 5
+    # x = 0
+    # y = 1
+    # stdscr.addstr(12, 0, str(curses.COLORS))
+    # try:
+    #     for i in range(0, curses.COLORS):
+    #         stdscr.addstr(y, x, '{0:5}'.format(i), curses.color_pair(i))
+    #         x = (x + 5) % maxx
+    #         if x == 0:
+    #             y += 1
+    # except curses.ERR:
+    #     pass
 
 
 def firstMatchPosition(searchString, text):
