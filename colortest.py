56c2f0a626a7393d3da65ed330ee2e736d428a35
CodeBishop <sbishop@longdays.com>
2018-03-06 01:35:59 -0800

    Experiment more with curses colors. Add some example photos of curses colors on sysrescue.

---
+++ colortest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+
+import curses
+from mdmSMART.utils import *
+
+
+def main(stdscr):
+    setupCursesUtils(stdscr)
+    printAt(20, 20, "TEST", curses.COLOR_BLUE)
+    # curses.start_color()
+    # curses.use_default_colors()
+    for i in range(1, 8):
+        curses.init_pair(i, i, 0)
+    # stdscr.addstr(0, 0, '{0} colors available'.format(curses.COLORS))
+    for i in range(0, 8):
+        stdscr.addstr(1, i*2, str(i), curses.color_pair(i))
+    for i in range(0, 8):
+        stdscr.addstr(2, i*2, str(i), curses.color_pair(i) | curses.A_REVERSE | curses.A_BOLD)
+
+    stdscr.refresh()
+    stdscr.getch()
+
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLUE)
+    stdscr.addstr(5, 4, '{0:5}'.format(i), curses.color_pair(1))
+    stdscr.refresh()
+    stdscr.getch()
+
+curses.wrapper(main)
+
+
+
+
+
--- mdmSMART/utils.py
+++ mdmSMART/utils.py
@@ -7,20 +7,15 @@ import subprocess
 import curses
 
 # Utility function constants.
-SEARCH_FAILED = -1
+# SEARCH_FAILED = -1
 
 # Open the null device for dumping unwanted output into.
-DEVNULL = open(os.devnull, 'w')
+# DEVNULL = open(os.devnull, 'w')
 
 CEC = "%%%"  # Escape code for colored or special text.
-CECLEN = len(CEC) + 2  # Total length of an escape code.
+CECLEN = len(CEC) + 1  # Total length of an escape code.
 
-# Prepare color pairs.
-# colorPair = list()
-# for i in range(0, 8):
-#     colorPairSublist = list()
-#     for j in range():
-#         colorPairSublist.append
+utilsWindow = None
 
 
 class ColoredString:
@@ -31,57 +26,54 @@ class ColoredString:
         return len(self.text) - self.text.count(CEC) * CECLEN
 
 
-def printAt(window, x, y, text):
-    window.addStr
-    curses.use_default_colors()
-    for i in range(0, curses.COLORS):
-        curses.init_pair(i + 1, i, i-1)
-    # stdscr.addstr(0, 0, '{0} colors available'.format(curses.COLORS))
-    # maxy, maxx = stdscr.getmaxyx()
-    # maxx = maxx - maxx % 5
-    # x = 0
-    # y = 1
-    # stdscr.addstr(12, 0, str(curses.COLORS))
-    # try:
-    #     for i in range(0, curses.COLORS):
-    #         stdscr.addstr(y, x, '{0:5}'.format(i), curses.color_pair(i))
-    #         x = (x + 5) % maxx
-    #         if x == 0:
-    #             y += 1
-    # except curses.ERR:
-    #     pass
+def setPrintWindow(activeWindow):
+    global utilsWindow
+    utilsWindow = activeWindow
 
 
-def firstMatchPosition(searchString, text):
-    searchResult = re.search(searchString, text)
-    if searchResult is None:
-        return SEARCH_FAILED
-    else:
-        return searchResult.start()
+def setupCursesUtils(activeWindow):
+    setPrintWindow(activeWindow)
+    # Prepare color pairs.
+    for i in range(0, 8):
+        curses.init_pair(i + 1, i, 0)
 
 
-def leftColumn(someString, width):
-    # Strip ANSI codes before calculating string length.
-    length = len(re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', someString))
-
-    # Left justify string, truncate (with ellipsis) or pad with spaces to fill column width.
-    if length <= width:
-        # NOTE: str.ljust() mis-formats this under some circumstances so don't use it.
-        return someString + ' ' * (width - length + 1)
-    else:
-        return someString[:width-3] + "... "
+def printAt(x, y, text):
+    utilsWindow.addstr(x, y, text, curses.color_pair(color))
+    # Split the string into a list of tuples of length, text and color/attribute)
+    # Loop through the list printing.
 
 
-# Get the output from a terminal command and block any error messages from appearing.
-def terminalCommand(command):
-    output, _ = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=DEVNULL).communicate()
-    return output
-
-
-# Use a regular expression to capture part of a string.
-def capture(pattern, text):
-    result = re.search(pattern, text, re.IGNORECASE)
-    if result and result.group:
-        return result.group(1)
-    else:
-        return ""
+# def firstMatchPosition(searchString, text):
+#     searchResult = re.search(searchString, text)
+#     if searchResult is None:
+#         return SEARCH_FAILED
+#     else:
+#         return searchResult.start()
+#
+#
+# def leftColumn(someString, width):
+#     # Strip ANSI codes before calculating string length.
+#     length = len(re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', someString))
+#
+#     # Left justify string, truncate (with ellipsis) or pad with spaces to fill column width.
+#     if length <= width:
+#         # NOTE: str.ljust() mis-formats this under some circumstances so don't use it.
+#         return someString + ' ' * (width - length + 1)
+#     else:
+#         return someString[:width-3] + "... "
+#
+#
+# # Get the output from a terminal command and block any error messages from appearing.
+# def terminalCommand(command):
+#     output, _ = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=DEVNULL).communicate()
+#     return output
+#
+#
+# # Use a regular expression to capture part of a string.
+# def capture(pattern, text):
+#     result = re.search(pattern, text, re.IGNORECASE)
+#     if result and result.group:
+#         return result.group(1)
+#     else:
+#         return ""
